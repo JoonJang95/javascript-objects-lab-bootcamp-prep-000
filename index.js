df109eb77ec8f5f4f943007f89554c00283af9cc
Joon Jang <joon.jang95@gmail.com>
2018-08-10 23:29:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function updateObjectWithKeyAndValue(recipe, key, value) {
   return Object.assign({}, recipe, { [key]: value })
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(recipe, key, value) {
+  recipe[key] = value 
+  
+  return recipe
+}
